559261011b8da8cbed9e3d2c3b56c39c5eded4e0
yaw.o.k <obengky72@gmail.com>
2024-11-30 22:18:22 +0100

    Configure config variables and database

--- api/shop-api/src/app.module.ts
+++ api/shop-api/src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { DatabaseConfig } from './config/database.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      useClass: DatabaseConfig
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
